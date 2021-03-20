40aee11538d029e08365e7ecb9d2242f27a2f2ed
jperelygin <jperelygin@gmail.com>
2021-03-20 13:40:42 +0300

    Add PI const to lib

--- lib.h
+++ lib.h
@@ -1,3 +1,4 @@
 #pragma once
 
 extern int build_number;
+const float pi = 3.14;
--- test.cpp
+++ test.cpp
@@ -8,6 +8,9 @@ BOOST_AUTO_TEST_CASE(Check)
 {
     BOOST_ASSERT(build_number > 0);
 }
-
+BOOST_AUTO_TEST_CASE(pi_check)
+{
+    BOOST_ASSERT(pi == 3.14)
+}
 
 BOOST_AUTO_TEST_SUITE_END()
